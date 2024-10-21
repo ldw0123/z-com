53116dd1035b8c74130d5bab8e8ba7704fe181d6
ldw0123 <ldw0123@gmail.com>
2024-10-22 03:19:23 +0900

    ✨ default.tsx

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -12,5 +12,5 @@ export default function Layout({ children, modal }: Props) {
   );
 }
 
-// 주소가 localhost:3001일 때는 children->page.tsx, modal->@modal/default.tsx
-// 주소가 localhost:3001/i/flow/login 때는 children->i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx
+// 주소가 localhost:3000일 때는 children->page.tsx, modal->@modal/default.tsx
+// 주소가 localhost:3000/i/flow/login 때는 children->i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx
